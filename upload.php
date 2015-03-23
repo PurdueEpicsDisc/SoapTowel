e2484dd3c9c6d1b1e455b410312beee9f27cf2ae
Joshua <jsulli0527@gmail.com>
2015-03-22 21:08:34 -0400

    Save and upload now working

--- upload.php
+++ upload.php
@@ -1,4 +1,25 @@
 <?php
-//Upload function code to allow when database goes down can upload all the data from the
-//external hard drive to refill database so the information isn't lost;
+//new upload.php
+$myfile = fopen("C:\\Users\\Joshua\\Desktop\\DatabaseSave.txt", "r");
+//open database connection
+$conn = new mysqli("localhost", "root", "", "soap_towel_campaign");
+
+while(!feof($myfile))
+{
+	//Remember no punctuation needed in insert into statement
+	//or will screw up the entire insert statement
+	$sql = "";
+	$str = fgets($myfile);
+	//String split with "-" delimiter into an array hopefully
+	//gets rid of newline character at the end
+	$strOne = str_replace(array("\r", "\n"), '', $str);
+	//Seperates all the different fields that were delimited by the '-' character.
+	$parts = (explode('-',$strOne,9));
+	//Creates sql statement so that it may be reuploaded into database.
+	$sql = "INSERT INTO homeless_shelters VALUES('" . $parts[0] . "','" . $parts[1] . "','" . $parts[2] . "','" . $parts[3] . "','" . $parts[4] . "','" . $parts[5] . "','" . $parts[6] . "','" . $parts[7] . "','" . $parts[8] . "')";
+	echo $sql . "<br />";
+	if($conn->query($sql) === TRUE)
+		echo "New Record Created";
+}
+fclose($myfile);
 ?>
