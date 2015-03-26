dc61154834716c08722e06aa13debe18c1673eec
Andrew Heil <aheil@purdue.edu>
2015-03-26 01:43:36 -0400

    AH - donationPage and donationInputPage changed to put values into an array then save them in the database as a text value, deliminating each value with a dash -

--- donationInputPage.php
+++ donationInputPage.php
@@ -4,12 +4,17 @@
 <!--GUI for inputing donation Page information-->
 <!--Once the user hits submit the information in the textbox fields are sent  donationPage.php seen in the line below-->
 <form action="donationPage.php" method="post">
+<?php
+
+$itemArray = array();
+
+?>
 Shelter Name: <input type="text" name="Shelter"><br><!--text box followed by line break-->
-Item: <input type="text" name="Item1"> Importance: <input type="text" name="Importance1"><br> <!--two text boxes followed by line break-->
-Item: <input type="text" name="Item2"> Importance: <input type="text" name="Importance2"><br>
-Item: <input type="text" name="Item3"> Importance: <input type="text" name="Importance3"><br>
-Item: <input type="text" name="Item4"> Importance: <input type="text" name="Importance4"><br>
-Item: <input type="text" name="Item5"> Importance: <input type="text" name="Importance5"><br>
+Item: <input type="text" name="itemArray[0]"> Importance: <input type="text" name="itemArray[1]"><br> <!--two text boxes followed by line break-->
+Item: <input type="text" name="itemArray[2]"> Importance: <input type="text" name="itemArray[3]"><br>
+Item: <input type="text" name="itemArray[4]"> Importance: <input type="text" name="itemArray[5]"><br>
+Item: <input type="text" name="itemArray[6]"> Importance: <input type="text" name="itemArray[7]"><br>
+Item: <input type="text" name="itemArray[8]"> Importance: <input type="text" name="itemArray[9]"><br>
 <input type="submit"> <!--submit button-->
 </form>
 
--- donationPage.php
+++ donationPage.php
@@ -12,16 +12,16 @@
 //This page does the work of submiting the data to the data base once it recieves input from donationInputPage.php
 //If page is accessed directly it submits an empty row in the database
 $Shelter = $_POST["Shelter"];
-$Item1 = $_POST["Item1"];
-$Item2 = $_POST["Item2"];
-$Item3 = $_POST["Item3"];
-$Item4 = $_POST["Item4"];
-$Item5 = $_POST["Item5"];
-$Importance1 = $_POST["Importance1"];
-$Importance2 = $_POST["Importance2"];
-$Importance3 = $_POST["Importance3"];
-$Importance4 = $_POST["Importance4"];
-$Importance5 = $_POST["Importance5"];
+$ItemArray = $_POST["itemArray"];
+$ItemString = "";
+for ($i = 0; $i < sizeof($ItemArray); $i++){
+	$ItemString .= $ItemArray[$i];
+	$ItemString .= "-";
+}
+
+$Item1 = $ItemString;
+
+
 
 //include 'connectDonationPage.php';
 //create connection
