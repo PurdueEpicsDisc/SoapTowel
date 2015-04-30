77e6b033b32a1e839598a1e13ec9ba5bb869f5d0
vaiyagar <vaiyagar@purdue.edu>
2015-04-30 12:04:03 -0400

    shelterInputPage

---
+++ shelterInputPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+<!--GUI for inputing donation Page information-->
+<form action="shelterPage.php" method="post">
+<?php
+
+?>
+Username: <input type="text" name="username"><br><!--One text box per line-->
+Password: <input type="text" name="password"><br>
+Shelter Name: <input type="text" name="name"><br>
+Address: <input type="text" name="address"><br>
+Zip Code: <input type="text" name="zipCode"><br>
+Phone Number: <input type="text" name="phone"><br>
+Email: <input type="text" name="email"><br>
+
+
+<input type="submit"> <!--submit button-->
+</form>
+
+</body>
+</html>
